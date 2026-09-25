393c0bd118bf623f4d4c2d7e2fb7bdf1598edbc7
ThanhNguyen051 <huynhthanhnguyenct2001@gmail.com>
2026-09-26 02:19:31 +0700

    commit

--- utils/geminiVision.js
+++ utils/geminiVision.js
@@ -38,11 +38,35 @@ async function analyzePokemonTeamImages(imageUrls) {
             contents: [
                 ...imageParts,
                 {
-                    text: `Hãy đọc chi tiết toàn bộ team Pokémon trong hình ảnh này 
+                    text: `Hãy đọc chi tiết toàn bộ team Pokémon trong hình ảnh này (Không đoán theo build trên internet, trả lời đúng theo form bên dưới mà không thêm bất kỳ text nào khác, không fotmat, không giải thích, không thêm bất kỳ ký tự nào khác ngoài nội dung yêu cầu).
                     - Nếu hình ảnh là Move&More, lấy ra tên Pokémon, ability, item và danh sách các chiêu thức của từng pokemon.
-                    - Nếu hình ảnh là Stats, lấy ra tên tính cách của từng pokemon dựa vào mũi tên tăng giảm của các chỉ số
+                    - Nếu hình ảnh là Stats, lấy ra tên tính cách của từng pokemon dựa vào mũi tên tăng giảm của các chỉ số. Adamant	Attack	Sp. Atk
+                        Bashful: tăng Sp. Atk, giảm Sp. Def
+                        Bold: tăng Defense, giảm Attack
+                        Brave: tăng Attack, giảm Speed
+                        Calm: tăng Sp. Def, giảm Attack
+                        Careful: tăng Sp. Def, giảm Sp. Atk
+                        Docile: tăng Defense, giảm Defense
+                        Gentle: tăng Sp. Def, giảm Defense
+                        Hardy: tăng Attack, giảm Attack
+                        Hasty: tăng Speed, giảm Defense
+                        Impish: tăng Defense, giảm Sp. Atk
+                        Jolly: tăng Speed, giảm Sp. Atk
+                        Lax: tăng Defense, giảm Sp. Def
+                        Lonely: tăng Attack, giảm Defense
+                        Mild: tăng Sp. Atk, giảm Defense
+                        Modest: tăng Sp. Atk, giảm Attack
+                        Naive: tăng Speed, giảm Sp. Def
+                        Naughty: tăng Attack, giảm Sp. Def
+                        Quiet: tăng Sp. Atk, giảm Speed
+                        Quirky: tăng Sp. Def, giảm Sp. Def
+                        Rash: tăng Sp. Atk, giảm Sp. Def
+                        Relaxed: tăng Defense, giảm Speed
+                        Sassy: tăng Sp. Def, giảm Speed
+                        Serious: tăng Speed, giảm Speed
+                        Timid: tăng Speed, giảm Attack
                     In ra chỉ số theo dạng
-                    
+
                 "Froslass-Mega @ Froslassite
                 Ability: Snow Warning
                 Timid Nature
